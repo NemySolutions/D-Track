58cd43264e56b1a22d7cb56202f17f46e5885711
Tan Ming Sheng <mingsheng36@gmail.com>
2017-10-27 11:24:29 +0800

    Added Home Page

--- Dev/digital-tracker/App.js
+++ Dev/digital-tracker/App.js
@@ -4,65 +4,50 @@ import { StackNavigator } from 'react-navigation'; // 1.0.0-beta.14
 
 const HomeScreen = ({ navigation }) => (
   <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between' }}>
-  <ScrollView>
-    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
-      <TouchableHighlight style={{ flex: 1, flexDirection: 'column' }} onPress={() => navigation.navigate('Inventory')}>
-        <Image source={require('./img/favicon.png')} resizeMode="contain">
-          <Text style={styles.buttonText}>TouchableHighlight</Text>
-        </Image>
-      </TouchableHighlight>
-      <TouchableHighlight style={{ flex: 1, flexDirection: 'column' }} onPress={() => navigation.navigate('Inventory')}>
-        <Image resizeMode="contain" source={require('./img/favicon.png')} >
-          <Text style={styles.buttonText}>TouchableHighlight</Text>
-        </Image>
-      </TouchableHighlight>
-    </View>
+   
+      <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
+        <TouchableHighlight style={{ flex: 1, flexDirection: 'column' }} onPress={() => navigation.navigate('Inventory')}>
+          <Image source={require('./img/inventory.jpg')} style={styles.image}>
+          <Text style={styles.homeTouchableText}>Inventory</Text>
+          </Image>
+        </TouchableHighlight>
+        <TouchableHighlight style={{ flex: 1, flexDirection: 'column' }} onPress={() => navigation.navigate('Inventory')}>
+          <Image source={require('./img/caterer.jpg')} style={styles.image}>
+          <Text style={styles.homeTouchableText}>Caterers</Text>
+          </Image>
+        </TouchableHighlight>
+      </View>
 
-    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
-      <TouchableHighlight style={{ flex: 1, flexDirection: 'column' }} onPress={() => navigation.navigate('Inventory')}>
-        <Image source={require('./img/favicon.png')} resizeMode="contain">
-          <Text style={styles.buttonText}>TouchableHighlight</Text>
-        </Image>
-      </TouchableHighlight>
-      <TouchableHighlight style={{ flex: 1, flexDirection: 'column' }} onPress={() => navigation.navigate('Inventory')}>
-        <Image resizeMode="contain" source={require('./img/favicon.png')} >
-          <Text style={styles.buttonText}>TouchableHighlight</Text>
-        </Image>
-      </TouchableHighlight>
-    </View>
+      <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
+        <TouchableHighlight style={{ flex: 1, flexDirection: 'column' }} onPress={() => navigation.navigate('Inventory')}>
+          <Image source={require('./img/flight.jpg')} style={styles.image}>
+          <Text style={styles.homeTouchableText}>Flights</Text>
+          </Image>
+        </TouchableHighlight>
+        <TouchableHighlight style={{ flex: 1, flexDirection: 'column' }} onPress={() => navigation.navigate('Inventory')}>
+          <Image source={require('./img/reallocate.jpg')} style={styles.image}>
+          <Text style={styles.homeTouchableText}>Reallocate</Text>
+          </Image>
+        </TouchableHighlight>
+      </View>
 
-    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
-      <TouchableHighlight style={{ flex: 1, flexDirection: 'column' }} onPress={() => navigation.navigate('Inventory')}>
-        <Image source={require('./img/favicon.png')} resizeMode="contain">
-          <Text style={styles.buttonText}>TouchableHighlight</Text>
-        </Image>
-      </TouchableHighlight>
-      <TouchableHighlight style={{ flex: 1, flexDirection: 'column' }} onPress={() => navigation.navigate('Inventory')}>
-        <Image resizeMode="contain" source={require('./img/favicon.png')} >
-          <Text style={styles.buttonText}>TouchableHighlight</Text>
-        </Image>
-      </TouchableHighlight>
-    </View>
-    
-    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
-      <TouchableHighlight style={{ flex: 1, flexDirection: 'column' }} onPress={() => navigation.navigate('Inventory')}>
-        <Image source={require('./img/favicon.png')} resizeMode="contain">
-          <Text style={styles.buttonText}>TouchableHighlight</Text>
-        </Image>
-      </TouchableHighlight>
-      <TouchableHighlight style={{ flex: 1, flexDirection: 'column' }} onPress={() => navigation.navigate('Inventory')}>
-        <Image resizeMode="contain" source={require('./img/favicon.png')} >
-          <Text style={styles.buttonText}>TouchableHighlight</Text>
-        </Image>
-      </TouchableHighlight>
-    </View>
-    
-    </ScrollView>
+      <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
+        <TouchableHighlight style={{ flex: 1, flexDirection: 'column' }} onPress={() => navigation.navigate('Inventory')}>
+          <Image source={require('./img/order.jpg')} style={styles.image}>
+          <Text style={styles.homeTouchableText}>Orders</Text>
+          </Image>
+        </TouchableHighlight>
+        <TouchableHighlight style={{ flex: 1, flexDirection: 'column' }} onPress={() => navigation.navigate('Inventory')}>
+          <Image source={require('./img/packing.jpg')} style={styles.image}>
+          <Text style={styles.homeTouchableText}>Packing Plan</Text>
+          </Image>
+        </TouchableHighlight>
+      </View>
+ 
     <StatusBar
       backgroundColor="blue"
       barStyle="light-content"
-      hidden={true}
-    />
+      hidden={true}/>
   </View>
 );
 
@@ -88,7 +73,24 @@ const RootNavigator = StackNavigator({
 });
 
 const styles = StyleSheet.create({
-
+  image: {
+    flex: 1,
+    opacity: 1000,
+    width: null,
+    height: null,
+    resizeMode: 'stretch',
+  },
+  homeTouchableText: {
+    flex: 1,
+    fontSize: 35,
+    textAlign: 'center',
+    fontWeight: '900',
+    color: 'aliceblue',
+    backgroundColor:'rgba(0,0,0,0.4)',
+    justifyContent:'center',
+    alignItems: 'center',
+    lineHeight: 140,
+  }
 })
 
 export default RootNavigator;
