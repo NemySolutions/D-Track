db3b7548163ef5c8a853b21cb3c55af9514d1d32
Tan Ming Sheng <mingsheng36@gmail.com>
2017-10-27 09:00:56 +0800

    Added nagviation

--- Dev/digital-tracker/App.js
+++ Dev/digital-tracker/App.js
@@ -1,22 +1,95 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, View, Text, Image, Button, StatusBar, StyleSheet, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback } from 'react-native';
+import { StackNavigator } from 'react-navigation'; // 1.0.0-beta.14
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Hello World</Text>
-      </View>
-    );
-  }
-}
+const HomeScreen = ({ navigation }) => (
+  <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between' }}>
+  <ScrollView>
+    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
+      <TouchableHighlight style={{ flex: 1, flexDirection: 'column' }} onPress={() => navigation.navigate('Inventory')}>
+        <Image source={require('./img/favicon.png')} resizeMode="contain">
+          <Text style={styles.buttonText}>TouchableHighlight</Text>
+        </Image>
+      </TouchableHighlight>
+      <TouchableHighlight style={{ flex: 1, flexDirection: 'column' }} onPress={() => navigation.navigate('Inventory')}>
+        <Image resizeMode="contain" source={require('./img/favicon.png')} >
+          <Text style={styles.buttonText}>TouchableHighlight</Text>
+        </Image>
+      </TouchableHighlight>
+    </View>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
+      <TouchableHighlight style={{ flex: 1, flexDirection: 'column' }} onPress={() => navigation.navigate('Inventory')}>
+        <Image source={require('./img/favicon.png')} resizeMode="contain">
+          <Text style={styles.buttonText}>TouchableHighlight</Text>
+        </Image>
+      </TouchableHighlight>
+      <TouchableHighlight style={{ flex: 1, flexDirection: 'column' }} onPress={() => navigation.navigate('Inventory')}>
+        <Image resizeMode="contain" source={require('./img/favicon.png')} >
+          <Text style={styles.buttonText}>TouchableHighlight</Text>
+        </Image>
+      </TouchableHighlight>
+    </View>
+
+    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
+      <TouchableHighlight style={{ flex: 1, flexDirection: 'column' }} onPress={() => navigation.navigate('Inventory')}>
+        <Image source={require('./img/favicon.png')} resizeMode="contain">
+          <Text style={styles.buttonText}>TouchableHighlight</Text>
+        </Image>
+      </TouchableHighlight>
+      <TouchableHighlight style={{ flex: 1, flexDirection: 'column' }} onPress={() => navigation.navigate('Inventory')}>
+        <Image resizeMode="contain" source={require('./img/favicon.png')} >
+          <Text style={styles.buttonText}>TouchableHighlight</Text>
+        </Image>
+      </TouchableHighlight>
+    </View>
+    
+    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
+      <TouchableHighlight style={{ flex: 1, flexDirection: 'column' }} onPress={() => navigation.navigate('Inventory')}>
+        <Image source={require('./img/favicon.png')} resizeMode="contain">
+          <Text style={styles.buttonText}>TouchableHighlight</Text>
+        </Image>
+      </TouchableHighlight>
+      <TouchableHighlight style={{ flex: 1, flexDirection: 'column' }} onPress={() => navigation.navigate('Inventory')}>
+        <Image resizeMode="contain" source={require('./img/favicon.png')} >
+          <Text style={styles.buttonText}>TouchableHighlight</Text>
+        </Image>
+      </TouchableHighlight>
+    </View>
+    
+    </ScrollView>
+    <StatusBar
+      backgroundColor="blue"
+      barStyle="light-content"
+      hidden={true}
+    />
+  </View>
+);
+
+const InventoryScreen = () => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>InventoryScreen Screen</Text>
+  </View>
+);
+
+const RootNavigator = StackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      headerTitle: 'Home',
+    },
+  },
+  Inventory: {
+    screen: InventoryScreen,
+    navigationOptions: {
+      headerTitle: 'Inventory',
+    },
   },
 });
+
+const styles = StyleSheet.create({
+
+})
+
+export default RootNavigator;
+
